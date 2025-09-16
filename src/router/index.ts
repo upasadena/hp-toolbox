e3b9408692e248b03d99251da5ed38c259760719
upasadena <underscore.pasadena@gmail.com>
2025-09-16 13:12:53 +0000

    fix?

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/hp-toolbox/' : '/'),
   routes: setupLayouts(routes),
 })
 
