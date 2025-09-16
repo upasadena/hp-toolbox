f32b11d8f3154d944bd9a1dbb16d1265aff4f45d
upasadena <underscore.pasadena@gmail.com>
2025-09-16 12:42:10 +0000

    try to fix part 2

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/hp-toolbox/' : '/',
+  publicPath: process.env.VITE_BASE_URL || '/',
 
   // Add this line to sync BASE_URL with publicPath
   configureWebpack: {
-    base: process.env.NODE_ENV === 'production' ? '/hp-toolbox/' : '/',
+    base: process.env.VITE_BASE_URL || '/',
   }
 };
 
 console.log('NODE_ENV:', process.env.NODE_ENV);
 console.log('publicPath will be:', process.env.NODE_ENV === 'production' ? '/hp-toolbox/' : '/');
+console.log('publicPath2 will be:', process.env.VITE_BASE_URL || '/');
